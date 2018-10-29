6f3c2c1365b42294469e96f1aea522509bfa8065
LanaZahrEddin <43419131+LanaZahrEddin@users.noreply.github.com>
2018-10-29 13:36:55 -0400

    finished editing physicsAndcollisions

--- LivesAndTimersLanaZ/main.lua
+++ LivesAndTimersLanaZ/main.lua
@@ -1,172 +1,176 @@
 --hide the status bar
 display.setStatusBar(display.HiddenStatusBar)
---sets the background colour
-display.setDefault("background", 6/255, 46/255, 124/255)
----VARIABLES FOR THE TIMER
+
+--set the background color
+display.setDefault( "background", 252/255, 5/255, 190/255 )
+--------------------------------------------------------
+-- LOCAL VARIABLES
+--------------------------------------------------------
+--create variables for the timer
 local totalSeconds = 5
 local secondsLeft = 5
 local clockText
 local countDownTimer
-local lives = 3
-local heart1
-local heart2
-local heart3
-local heart4
+local gameOver
+local lives = 4
+
+--create other local variables 
 local questionObject
+local correctObject
+local incorrectObject
 local numericField
 local randomNumber1
 local randomNumber2
+local randomOperator
 local userAnswer
 local incorrectAnswer
 local correctAnswer
 local pointsTextObject
-local numberPoints = 0
-local randomOperator
---Correct Sound
+local numberOfPoints = 0
+
+--------------------------------------------------------
+-- SOUNDS
+--------------------------------------------------------
+--correct sound
 local correctSound = audio.loadSound("Sounds/correctSound.mp3")
 local correctSoundChannel
 --Incorrect Sound
 local wrongSound = audio.loadSound("Sounds/wrongSound.mp3")
-local wrongSoundChannel 
+local wrongSoundChannel
+--train(background)
+local trainSound = audio.loadSound("Sounds/train.mp3")
+local trainSoundChannel
 
--- local functions 
-
---***ADD LOCAL VARIABLE FOR: INCORRECT OBJECT, POINTS
 --------------------------------------------------------
 -- LOCAL FUNCTIONS
 --------------------------------------------------------
+local function UpdateHearts()
+ if (lives == 4) then
+      heart1.isVisible = true
+      heart2.isVisible = true
+      heart3.isVisible = true
+      heart4.isVisible = true
+
+     elseif (lives == 3) then
+      heart1.isVisible = true
+      heart2.isVisible = true
+      heart3.isVisible = true
+      heart4.isVisible = false
+  
+     elseif (lives == 2) then
+      heart1.isVisible = true
+      heart2.isVisible = true
+      heart3.isVisible = false
+      heart4.isVisible = false
+
+     elseif (lives == 1) then
+      heart1.isVisible = true
+      heart2.isVisible = false
+      heart3.isVisible = false
+      heart4.isVisible = false
+
+     elseif (lives == 0) then
+      heart1.isVisible = false
+      heart2.isVisible = false
+      heart3.isVisible = false
+      heart4.isVisible = false
+      gameOver.isVisible = true
+      trainSoundChannel = audio.play(trainSound)
+      numericField.isVisible = false
+      pointsTextObject.isVisible = false
+      questionObject.isVisible = false
+     end
+end
+-------------------------------------------------------------
 local function UpdateTime()
  -- decrement the number of sounds left
  secondsLeft = secondsLeft - 1
  --display the number of seconds left in the
  --clock object
  clockText.text = secondsLeft .. ""
- if(secondsLeft == 0 ) then
+ if (secondsLeft == 0 ) then
   --reset the  number of seconds left 
   secondsLeft = totalSeconds
   lives = lives - 1
   --***IF THERE ARE NO LIVES LEFT, PLAY A LOSE SOUND,
   --SHOW A "YOU LOSE IMAGE AND CANCEL THE TIMER, REMOVE
   --REMOVE THE THIRD HEART BY MAKIND IT INVISIBLE
-  if (lives == 2) then
-   heart2.isVisible = false
-  elseif (lives == 1) then
-   heart1.isVisible = false
-  end
+     UpdateHearts()
   --*** CALL THE FUNCTION TO ASK A NEW QUESTION
  end
 end
-
+------------------------------------------------------------------------
 --function that calls the timer
 local function StartTime()
  -- create a countdown timer that loops infinitely
  countDownTimer = timer.performWithDelay( 1000, UpdateTime, 0)
 end
-------------------------------------------------------------
+
+-- local functions 
 local function AskQuestion()
-	--generate 2 random numbers between a max. and a min. number
-	randomOperator = math.random(1,3)
-	randomNumber1 = math.random(10,20)
-	randomNumber2 = math.random(10,20)
-
+ --generate 2 random numbers between a max. and a min. number
+ randomOperator = math.random(1,3)
+ randomNumber1 = math.random(10,20)
+ randomNumber2 = math.random(10,20)
  
- 	if (randomOperator == 1) then
- 		correctAnswer = randomNumber1 + randomNumber2
-	
-		--create questionin text object
-		questionObject.text = randomNumber1 .. " + " .. randomNumber2 .. " = "
-
-	elseif (randomOperator == 2) then
-	 	correctAnswer = randomNumber1 - randomNumber2
-
-		--create questionin text object
-		questionObject.text = randomNumber1 .. " - " .. randomNumber2 .. " = "
-
-	elseif (randomOperator == 3) then
-	 	correctAnswer = randomNumber1 * randomNumber2
-
-		--create questionin text object
-		questionObject.text = randomNumber1 .. " * " .. randomNumber2 .. " = "
-	end	
-  
+  if (randomOperator == 1) then
+   correctAnswer = randomNumber1 + randomNumber2
+  --create question in text object
+  questionObject.text = randomNumber1 .. " + " .. randomNumber2 .. " = "
+ elseif (randomOperator == 2) then
+   correctAnswer = randomNumber1 - randomNumber2
+  --create question in text object
+  questionObject.text = randomNumber1 .. " * " .. randomNumber2 .. " = "
+ elseif (randomOperator == 3) then
+   correctAnswer = randomNumber1 * randomNumber2
+  --create question in text object
+  questionObject.text = randomNumber1 .. " - " .. randomNumber2 .. " = "
+ end 
 end
---------------------------------------------------------
+
+
+local function HideCorrect()
+ correctObject.isVisible = false
+ AskQuestion()
+end
+
+local function HideIncorrect()
+ incorrectObject.isVisible = false 
+ AskQuestion()
+end
+
+
 local function NumericFieldListener( event )
-
-	--User beging editing "numericfeild"
-	if ( event.phase == "began" ) then
-
-		--clear text field 
-		event.target.text = "" 
-
-	elseif event.phase == "submitted" then
-
-		-- when the answer is submitted (enter key is pressed) set user input bto user's answer
-		userAnswer = tonumber(event.target.text)
-
-		--if the users answer and the correct answer and the correct answer are the same:
-		if (userAnswer == correctAnswer) then
-			correctObject.isVisible = true 
-			correctSoundChannel = audio.play(correctSound)
-			incorrectObject.isVisible = false
-			timer.performWithDelay(2000,HideCorrect)
-			numberPoints = numberPoints + 1
-
-			event.target.text = "" 
-
+ --User begins editing "numericFeild"
+ if ( event.phase == "began" ) then
+  --clear text field 
+  event.target.text = ""
+ elseif event.phase == "submitted" then
+  -- when the answer is submitted (enter key is pressed) set user input bto user's answer
+  userAnswer = tonumber(event.target.text)
+  --if the user's answer and the correct answer and the correct answer are the same:
+  if (userAnswer == correctAnswer) then
+   correctObject.isVisible = true 
+   correctSoundChannel = audio.play(correctSound)
+   incorrectObject.isVisible = false
+   timer.performWithDelay(2000,HideCorrect)
+   numberOfPoints = numberOfPoints + 1
+   event.target.text = ""
         -- create increasing points in the text object
-			 pointsTextObject.text = "Points = ".. numberPoints
-
-		else 
-			heart4.isVisible = false 
-			wrongSoundChannel = audio.play(wrongSound)
-			incorrectObject.isVisible = true
-			
-
-			event.target.text = "" 
-
-			
-		end
-
-	end
-
+    pointsTextObject.text = "Points = ".. numberOfPoints
+  elseif (userAnswer ~= correctAnswer) then
+   incorrectObject.isVisible = true
+   wrongSoundChannel = audio.play(wrongSound)
+   lives = lives - 1
+   UpdateHearts()
+   incorrectObject.isVisible = true
+   timer.performWithDelay(2000,HideIncorrect)
+   event.target.text = ""   
+  end
+ end
 end
-
-
---------------------------------------------------------
--- create points box adn make it visible
-pointsTextObject = display.newText( "Points = ".. numberPoints, 230, 100, nil, 70 )
-pointsTextObject:setTextColor(3/255, 3/255, 43/255)
-
---object Creation 
--- display a question and sets the color 
-questionObject = display.newText("", display.contentWidth/2, display.contentHeight/2, nil, 180)
-questionObject:setTextColor(124/255, 116/255, 114/255)
-
--- create the correct  text object and make it invisible
-correctObject = display.newText("Correct,YAHOOO!", display.contentWidth/2, 250, nil, 50)
-correctObject:setTextColor(55/255, 42/255, 198/255)
-correctObject.isVisible = false
-
--- create the incorrect  text object and make it invisible
-incorrectObject = display.newText("NOO,incorrect!", display.contentWidth/2, 250, nil, 50)
-incorrectObject:setTextColor(155/255, 2/255, 98/255)
-incorrectObject.isVisible = false
-
--- create numeric field 
-
-numericField = native.newTextField(display.contentWidth/2, display.contentHeight*2/3, 600, 95)
-numericField.inputType = "default"
-
---add the event listener fo the numeric field
-numericField:addEventListener( "userInput", NumericFieldListener)
-
--- function calls
--- call the functionto as the Question
-AskQuestion()
-
 ------------------------------------------------------------
---OBJECT CREATION
+--OBJECTS CREATION
 ------------------------------------------------------------
 --create the lives to display on the screen
 heart1 = display.newImageRect("Images/heart.png", 100,100)
@@ -181,3 +185,38 @@ heart3.y = display.contentHeight * 1 / 7
 heart4 = display.newImageRect("Images/heart.png", 100, 100)
 heart4.x = display.contentWidth * 4 / 8
 heart4.y = display.contentHeight * 1 / 7
+--display the timer on the screen
+clockText = display.newText(secondsLeft, 520, 680, nil, 190)
+clockText:setFillColor( 144/255, 234/255, 86/255 )
+--create and display game over on the screen
+gameOver = display.newImageRect("Images/gameOver.png", display.contentWidth, display.contentHeight)
+gameOver.anchorX = 0
+gameOver.anchorY = 0
+gameOver.isVisible = false
+-- create points box and make it visible
+pointsTextObject = display.newText( "Points = ".. numberOfPoints, 200, 100, nil, 60 )
+pointsTextObject:setTextColor(24/255, 119/255, 25/255)
+-- display a question and sets the color 
+questionObject = display.newText("", display.contentWidth/2, display.contentHeight/2, nil, 150)
+questionObject:setTextColor(10/255, 10/255, 70/255)
+-- create the correct  text object and make it invisible
+correctObject = display.newText("Yeyy,Correct!", display.contentWidth/2, 250, nil, 60)
+correctObject:setTextColor(10/255, 50/255, 226/255)
+correctObject.isVisible = false
+-- create the incorrect  text object and make it invisible
+incorrectObject = display.newText("Oh Oh,Incorrect!", display.contentWidth/2, 250, nil, 50)
+incorrectObject:setTextColor(4/255, 224/255, 19/255)
+incorrectObject.isVisible = false
+-----------------------------------------------------------
+-- CREATE THE NUMERIC FIELD 
+------------------------------------------------------------
+numericField = native.newTextField(display.contentWidth/2, display.contentHeight*2/3, 400, 100)
+numericField.inputType = "default"
+--add the event listener fo the numeric field
+numericField:addEventListener( "userInput", NumericFieldListener)
+---------------------------------------------------------
+-- FUNCTION CALLS
+----------------------------------------------------------
+-- call the function to ask the Question
+AskQuestion()
+StartTime()
--- PhysicsAndCollisionsLanaZ/main.lua
+++ PhysicsAndCollisionsLanaZ/main.lua
@@ -1,7 +1,134 @@
 -----------------------------------------------------------------------------------------
---
--- main.lua
---
+--Title: Interacting Onject 2
+--Name: Lana ZahrEddin
+--Course: ICS3C
+--This program displays different sized balls falling on a beam.
 -----------------------------------------------------------------------------------------
 
--- Your code here
+-- Hide the status a=bar
+display.setStatusBar(display.HiddenStatusBar)
+
+--load physics
+local  physics = require("physics")
+
+--start physics
+physics.start()
+-------------------------------------------------------------------
+--OBJECTS--
+-------------------------------------------------------------------
+--Ground
+local ground = display.newImage("Images/ground.png", 0, 740)
+
+--change the width to be the same as the screen
+ground.width = display.contentWidth
+ground.x = display.contentWidth/2
+
+--add to physics
+physics.addBody(ground, "static", {friction=0.5, bounce=0.3})
+---------------------------------------------------------------------
+local beam = display.newImage("Images/beam.png", 0, 0)
+beam.width = display.contentWidth/2
+beam.height = display.contentHeight*1/9
+--set the x and y pos
+beam.x = display.contentCenterX/5
+beam.y = display.contentCenterY*1.65
+
+--rotate the beam -60 degrees so its on an angle 
+beam:rotate(45)
+
+--send it to the back layer
+beam:toBack()
+
+--add to physics 
+physics.addBody(beam, "static", {friction=0.5, bounce=0.3})
+----------------------------------------------------------
+--create second beam
+local beam2 = display.newImage("Images/beam.png", 900, 700)
+beam2.width = display.contentWidth/6
+beam2.height = display.contentHeight*2
+
+--send it to back
+beam2:toBack()
+
+--add to physics
+physics.addBody(beam2, "static", {friction=0.5, bounce})
+----------------------------------------------------------
+--create bkg
+local bkg = display.newImage("Images/bkg.png", 0, 0)
+
+	--set the x and the y pos
+	bkg.x = display.contentCenterX
+	bkg.y = display.contentCenterY
+	bkg.width = display.contentWidth
+	bkg.height = display.contentHeight
+
+	--send to back
+	bkg:toBack()
+
+	-----------------------------------------------------------
+	--FUNCTIONS
+	-----------------------------------------------------------
+
+--create the first ball
+local function firstBall()
+		-- creating first ball
+		local ball1 = display.newImage("Images/super_ball.png", 0, 0)
+		--add to physics
+	 	physics.addBody(ball1, {density= 2.0, friction=1.5, bounce=0.3, radius= 25})
+	end
+	------------------------------------------------------------------------------------------------------------
+
+	--create the second ball
+	local function secondBall()
+		-- creating second ball
+		local ball2 = display.newImage("Images/super_ball.png", 0, 0)
+		--add to physics
+		physics.addBody(ball2, {density=1,0, friction=0.5, bounce=1.1, radius=12.5})
+		ball2:scale(0.5, 0.5)
+	end
+	-------------------------------------------------------------------------------------------------------------
+	--create the third ball
+	local function thirdBall()
+		-- creating third ball
+		local ball3 = display.newImage("Images/super_ball.png", 0, 0)
+		ball3:scale(2, 2)
+	
+		--add to physics
+		physics.addBody(ball3, {density=1,7, friction=9.5, bounce=0.7, radius=50})
+	end
+	--------------------------------------------------------------------------------------------------------------
+	--create the fourth ball
+	local function fourthBall()
+		-- creating fourth ball
+		local ball4 = display.newImage("Images/super_ball.png", 0, 0)
+		ball4:scale(4, 4)
+	
+		--add to physics
+		physics.addBody(ball4, {density=1,0, friction=0.8, bounce=0.7, radius=100})
+	end
+	--------------------------------------------------------------------------------------------------------------
+	--create the fifthball
+	local function fifthBall()
+		-- creating fifth ball
+		local ball5 = display.newImage("Images/super_ball.png", 0, 0)
+		--add to physics
+		physics.addBody(ball5, {density=1,0, friction=0.8, bounce=0.7, radius=200})
+		ball5:scale(8, 8)
+	end
+
+	--------------------------------------------------------------
+	--Sounds
+	--------------------------------------------------------------
+	local backgroundSound = audio.loadSound("Sounds/background.mp3")
+	local backgroundSoundChannel
+
+	---------------------------------------------------------------
+	--TIMER DELAYS - CALL EACH FUNCTION AFTER THE GIVEN MILLISECOND
+	---------------------------------------------------------------
+	timer.performWithDelay(0, firstBall)
+	timer.performWithDelay(500, secondBall)
+	timer.performWithDelay(1000, thirdBall)
+	timer.performWithDelay(1500, thirdBall)
+	timer.performWithDelay(2000, thirdBall)
+
+	backgroundSoundChannel = audio.play(backgroundSound)
